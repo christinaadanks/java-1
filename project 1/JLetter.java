7f14d5283f96756de726293d08bfeb1f5fd3d94b
christinaadanks <christinaa.danks@me.com>
2021-03-18 00:06:20 -0700

    Add files via upload

---
+++ project 1/JLetter.java
@@ -0,0 +1,30 @@
+/**
+ * Program to print the letter J as a large block letter.
+ *
+ * Project 1
+ * @author Christinaa Danks
+ * @version 8/19/2020
+ */
+
+public class JLetter {
+
+   /**
+    * Prints the letter J as a large block letter.
+    *
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args) {
+   
+      // Print block letter J.
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+   }
+}
---
+++ project 1/MyLifeGoals.java
@@ -0,0 +1,41 @@
+/**
+ * Program to print short, medium, and long-term goals.
+ *
+ * Project 1
+ * @author Christinaa Danks
+ * @version 8/19/2020
+ */
+public class MyLifeGoals {
+
+   /**
+    * Prints name, short, medium, and long-term goals.
+    * @param args Command line arguments - not used.
+    */
+   public static void main(String[] args) {
+      
+      // Print first and last name.
+      System.out.println("Christinaa Danks");
+      System.out.println();
+      
+      // Print short-term life goals.
+      System.out.println("My short-term life goal is to finally earn my "
+         + "bachelor's degree in a field I actually enjoy. "
+         + "No one in my family has a college degree so this will be an "
+         + "accomplishment for myself and everyone I love.");   
+               
+      // Print mid-term life goals.
+      System.out.println("My mid-term life goal is to find a job in "
+         + "the tech industry and hopefully get the "
+         + "opportunity to work at a large tech company. The jobs "
+         + "I held before were not challenging enough and I felt "
+         + "extremely stuck. I have high hopes for the future.");
+         
+      // Print long-term life goals.
+      System.out.println("My long-term life goal is to build and create "
+         + "video games. I have always been amazed by the gaming "
+         + "industry and how they are able to make such realistic "
+         + "graphics and immersive experiences for gamers. One day "
+         + "I will create the next minecraft and I have Auburn to "
+         + "thank for that.");
+   }
+}

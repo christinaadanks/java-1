4912b1a9e76914d6ee66498c7b60583a0f93377a
christinaadanks <christinaa.danks@me.com>
2021-03-18 00:04:59 -0700

    Add files via upload

---
+++ activity 1/StudentInfo.java
@@ -0,0 +1,23 @@
+/**
+* Simple program to print student information.
+*
+* Activity 1
+* @author Christinaa Danks
+* @version 8/18/2020
+*/
+public class StudentInfo
+{
+   /**
+   * Prints student information.
+   * @param args Command line arguments - not used.
+   */
+   
+   public static void main(String[] args)
+   {
+      System.out.println("Name: Christina Liu");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+   }
+
+
+}
